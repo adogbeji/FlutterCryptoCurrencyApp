038f15f68fda5d785881bf5d48006cae701f3039
adogbeji <baka_1993@yahoo.co.uk>
2024-11-09 10:19:12 +0000

    Remove DEBUG banner & add app bar to home screen

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -5,7 +5,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: CircleAvatar(
+          child: Icon(Icons.person),
+        ),
+      ),
       body: Center(
         child: Text('Home Screen'),
       ),
