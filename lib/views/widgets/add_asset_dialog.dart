2f34bb5818c450a2d5a7f00d4e86f8ac933be15b
adogbeji <baka_1993@yahoo.co.uk>
2024-11-12 20:34:33 +0000

    Add starting files for AddAssetDialog widget

---
+++ lib/views/widgets/add_asset_dialog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddAssetDialog extends StatelessWidget {
+  const AddAssetDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Dialog Widget'),
+      ),
+    );
+  }
+}
