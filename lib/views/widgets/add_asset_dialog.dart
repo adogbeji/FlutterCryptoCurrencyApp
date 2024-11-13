7eee9954915d976a9e6dd4a48d4e246960c7ce5b
adogbeji <baka_1993@yahoo.co.uk>
2024-11-13 10:48:48 +0000

    Start buildiong AddAssetDialog widget

--- lib/views/widgets/add_asset_dialog.dart
+++ lib/views/widgets/add_asset_dialog.dart
@@ -5,9 +5,12 @@ class AddAssetDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Dialog Widget'),
+    return Container(
+      width: MediaQuery.sizeOf(context).width * 0.80,
+      height: MediaQuery.sizeOf(context).height * 0.40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
       ),
     );
   }
