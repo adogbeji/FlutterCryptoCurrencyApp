4cd85df60bcdd83922eb7c48009fb53909ad75a8
adogbeji <baka_1993@yahoo.co.uk>
2024-11-22 10:36:53 +0000

    Configure all requests to API in HTTPService class

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -1,5 +1,19 @@
+import 'package:crypto_currency_app/consts.dart';
 import 'package:dio/dio.dart';
 
 class HTTPService {
   final Dio _dio = Dio();  // Stores dio package
+
+  HTTPService() {
+    _configureDio();
+  }
+
+  _configureDio() {
+    _dio.options = BaseOptions(
+      baseUrl: 'https://api.cryptorank.io/v1/',
+      queryParameters: {
+        'api_key': CRYPTO_RANK_API_KEY,
+      },
+    );
+  }
 }
